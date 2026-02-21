dc2a7dcabdd73ea9a33b3a72b3fc0557c92749d0
DaikiKuromi <gustavophg07@gmail.com>
2026-02-21 16:42:58 -0300

    descobri os dados do openseeface

--- ReceberDados.gd
+++ ReceberDados.gd
@@ -1,14 +1,29 @@
 extends Node3D
 
 var host
-
+var leitor
 #PacketPeerUDP é usado em openseeface, ele não utiliza StreamPeerTCP, então StreamPeerTCP não funciona
 
 func _ready() -> void: #aqui é o bloco que roda uma vez pra estabelecer conexão, etc
 	host = PacketPeerUDP.new()
 	host.bind(11573) #essa porta é a do openseeface padrão genérica
+	leitor = StreamPeerBuffer.new()
 
 func _process(_float) -> void:
+	
+	leitor.seek(0)
+	
 	if host.get_available_packet_count() > 0:
-		print("pegando dados")
-		print(host.get_packet().size())
+		var dados = host.get_packet()
+		leitor.data_array = dados
+		var tempo = leitor.get_double()
+		print(tempo)
+
+		var ID = leitor.get_32()
+		print(ID)
+		var teste = leitor.get_float()
+		print(teste)		
+		var teste2 = leitor.get_float()
+		print(teste2)
+		var teste3 = leitor.get_float()
+		print(teste3)		
